23cc900b6028aba56ce75795bf1effc58d8969cb
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-10-02 06:35:59 +0330

    Fetch Projects from API in Home Page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const res = await fetch("/api/project/get-all");
+      const data = await res.json();
+
+      setProjects(data?.data);
+    };
+
+    fetchProjects();
+  }, []);
+
   return <div>Home</div>;
 };
 

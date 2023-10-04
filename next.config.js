9b869c67a618518454a09725182edec1c1ea878e
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-10-04 06:35:49 +0330

    eslint ignore during build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
